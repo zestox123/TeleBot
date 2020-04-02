b6ded519cf1c62e5b607d2ee3dbd13af6202739b
zestox123 <62999403+zestox123@users.noreply.github.com>
2020-04-02 14:51:02 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xditya\n"
-                     "`Bot created by:` [Aditya](https://t.me/xditya)\n"
+    await alive.edit("**`Hey crackexy! I'm alive. Crackexy ke baccho kaise ho?"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/xditya/TeleBot)")
+                     f"`My legit owner`: {DEFAULTUSER}\n"
